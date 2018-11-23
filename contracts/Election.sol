aca3d1f24e3d509cc5ef2c63ea9b44a9ffca7ad4
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2018-11-23 10:08:56 +0530

    voting in client side

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
         string name;
         uint voteCount;
     }
+
+    // storing the accounts that have already voted
+    mapping(address=>bool) public voters;
     // store a candidate
     mapping(uint=>Candidate) public candidates; // creating the variable of that mapping
     // fetch candidate 
@@ -28,5 +31,16 @@ contract Election {
         candidatesCount++; // this will increment the candidate counter
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); // assiging value to the candidate structure
     }
+    function vote(uint _candidateId) public{
+        //check that the address has not voted before
+        require(!voters[msg.sender]);
 
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        // record that voter has voted
+        voters[msg.sender] = true; // msg.sender has the account of the voter
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
+    }
 }
